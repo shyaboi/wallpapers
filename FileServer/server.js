bcc986a97b66923af2cee7dda8798280abb0dca6
Ian Sears <e@Unknown.domain>
2020-06-22 23:27:31 -0700

    node server succsessfully uploading files to folder

---
+++ FileServer/server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    // date stamp
+    var date = new Date(Date.now())
+       console.log(date);
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+       
+      var oldpath = files.filetoupload.path;
+      var newpath = date + 'Walls' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(4000);
