c88476f2d5a4ceb97436a03c917c2f2b6ee79336
Ian Sears <e@Unknown.domain>
2020-06-23 00:28:27 -0700

    Updates

---
+++ precache-manifest.7976599ae1271361dcc5eb0dea418ac0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9de5f0b7859186ea07582f04296a36",
+    "url": "/github.io/wallpapers/index.html"
+  },
+  {
+    "revision": "d574fb8c71aa11c704fd",
+    "url": "/github.io/wallpapers/static/css/main.51146142.chunk.css"
+  },
+  {
+    "revision": "d22d0198fcf56f42331e",
+    "url": "/github.io/wallpapers/static/js/2.59fafd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/wallpapers/static/js/2.59fafd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d574fb8c71aa11c704fd",
+    "url": "/github.io/wallpapers/static/js/main.8dec7111.chunk.js"
+  },
+  {
+    "revision": "98208647ef0cd8149226",
+    "url": "/github.io/wallpapers/static/js/runtime-main.3ee4e4c8.js"
+  },
+  {
+    "revision": "a87867af6f5a325b64c7730970dc834c",
+    "url": "/github.io/wallpapers/static/media/ok.a87867af.jpg"
+  },
+  {
+    "revision": "6d5d90959e46f2cb5f4fed468e4b60d7",
+    "url": "/github.io/wallpapers/static/media/ok2.6d5d9095.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wallpapers/precache-manifest.4ffffb164e970e140c028fbadcf63f6e.js"
+  "/github.io/wallpapers/precache-manifest.7976599ae1271361dcc5eb0dea418ac0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wallpapers/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/wallpapers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
