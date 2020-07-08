a9600a0c0356aa446473c5df21148a94c9a1962a
Shyaboi <ian.shane.sears@gmail.com>
2020-07-07 22:54:53 -0700

    Updates

---
+++ precache-manifest.764466047ead2db0ecdc5dc120416ce6.js
@@ -0,0 +1,65 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665cb8ad7e95eea798f00afb01b0c5b7",
+    "url": "/wallpapers/index.html"
+  },
+  {
+    "revision": "1d7a1df53fda71f0faf2",
+    "url": "/wallpapers/static/css/main.a3ccc401.chunk.css"
+  },
+  {
+    "revision": "f62ddb9a63f8ab0f165f",
+    "url": "/wallpapers/static/js/2.789c8dec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wallpapers/static/js/2.789c8dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7a1df53fda71f0faf2",
+    "url": "/wallpapers/static/js/main.52326ab4.chunk.js"
+  },
+  {
+    "revision": "6416ecb8b2ed5b9b3bbe",
+    "url": "/wallpapers/static/js/runtime-main.b0eff253.js"
+  },
+  {
+    "revision": "647b9ee825bf86a9f08aa27932500707",
+    "url": "/wallpapers/static/media/Walls01489254169675.647b9ee8.jpg"
+  },
+  {
+    "revision": "6ec3c3fff2250c39765e5ed481c0c561",
+    "url": "/wallpapers/static/media/Walls11493735596138.6ec3c3ff.jpg"
+  },
+  {
+    "revision": "a98f464399cd2a61b21d630cce1593ee",
+    "url": "/wallpapers/static/media/Walls11499561496815.a98f4643.jpg"
+  },
+  {
+    "revision": "930a91e866c98c02896e37157718e421",
+    "url": "/wallpapers/static/media/Walls120.930a91e8.jpg"
+  },
+  {
+    "revision": "1f37fb57f407eb6cdcc2d9b8179c8592",
+    "url": "/wallpapers/static/media/Walls125.1f37fb57.jpg"
+  },
+  {
+    "url": "/wallpapers/static/media/Walls178badffb60df1aa94872fe0c71ae47eb.78badffb.jpg"
+  },
+  {
+    "revision": "1e61aa61b97fa6465cd8cbc2e230f7a7",
+    "url": "/wallpapers/static/media/Walls1RJNONOo.1e61aa61.jpg"
+  },
+  {
+    "revision": "34c7c79b2bc1109c61b54d75839c1066",
+    "url": "/wallpapers/static/media/Walls21492050815553.34c7c79b.jpg"
+  },
+  {
+    "revision": "467948a0e325c40212111af7fff06d89",
+    "url": "/wallpapers/static/media/Walls_MUH8555.467948a0.jpg"
+  },
+  {
+    "revision": "f4c26689cf5ec8b160cea5cd41be8e94",
+    "url": "/wallpapers/static/media/Walls_MUH8694.f4c26689.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/wallpapers/precache-manifest.e65e3bec788488d8cf2ae3b7347f9efa.js"
+  "/wallpapers/precache-manifest.764466047ead2db0ecdc5dc120416ce6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/wallpapers/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wallpapers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
