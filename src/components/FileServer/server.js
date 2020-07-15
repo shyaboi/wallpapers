836f36c287326b6a695392e7b432b1ab11003dcd
Shyaboi <ian.shane.sears@gmail.com>
2020-07-14 21:28:26 -0700

    moving to express

--- src/components/FileServer/server.js
+++ src/components/FileServer/server.js
@@ -6,21 +6,30 @@ var express = require('express')
 var app = express()
 var PORT = process.env.port || 4000;
 const homePORT = process.env.PORT || 3000
-// const exPORT = process.env.PORT || 8080
+const exPORT = process.env.PORT || 8080
 
 
-// app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname+'../../public/index.html'));
-// })
+app.get('/', (req, res) => {
+  res.redirect('http://localhost:'+homePORT)
+})
 
-// app.get('*', (req,res) =>{
-//   res.send('bigoofs')
-// });
+app.get('/upload', (req, res) => {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+})
 
-// app.listen(exPORT)
-// console.log("Server Started on " + exPORT)
+app.get('/donus', (req,res) =>{
+  res.send('bigoofs')
+});
+
+app.listen(exPORT)
+console.log("Server Started on " + exPORT)
 
 try {
   const arrayOfFiles = fs.readdirSync("../FileServer/img")
@@ -30,7 +39,7 @@ try {
 
 
 let counter = 0
-http.createServer(function (req, res) {
+app.post("/fileupload", function (req, res) {
     // date stamp var
     var date = new Date(Date.now())
       //  console.log(req);
